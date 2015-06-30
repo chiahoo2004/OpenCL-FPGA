a1276d67dcb64e740ffb7f7eb2957d7beb8af854
chiahoo <b01901126@ntu.edu.tw>
2015-06-30 22:30:42 +0800

    delete redundant files

--- src/main.cpp
+++ src/main.cpp
@@ -119,7 +119,6 @@ int main(int argc, char** argv)
 	// OpenCL
 //	device_manager->GetKernel("bilateral.cl", "bilateral"); // preload the kernel
 	device_manager->GetKernel("gaussian1d.cl", "gaussian1d"); // preload the kernel
-//	device_manager->GetKernel("gaussian1dtwo.cl", "gaussian1dtwo");
 //	device_manager->GetKernel("guided.cl", "guided");
 //	device_manager->GetKernel("guidedtwo.cl", "guidedtwo");
 	tic = GetNow();
